67967767224853eb34ea46dad4acea9c9371b498
Ana Saragossa <aj.nenha@gmail.com>
2026-06-01 19:52:01 -0400

    Fix imports

--- core/design_system/src/main/kotlin/com/anenha/superhero/core/designsystem/component/BidirectionalPowerBar.kt
+++ core/design_system/src/main/kotlin/com/anenha/superhero/core/designsystem/component/BidirectionalPowerBar.kt
@@ -1,12 +1,17 @@
 package com.anenha.superhero.core.designsystem.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
