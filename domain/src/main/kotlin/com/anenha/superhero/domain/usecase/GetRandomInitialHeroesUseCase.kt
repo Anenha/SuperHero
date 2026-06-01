4353931599ac506999f00df4a56304cde4e69654
Ana Saragossa <aj.nenha@gmail.com>
2026-06-01 19:40:02 -0400

    Fix warning

--- domain/src/main/kotlin/com/anenha/superhero/domain/usecase/GetRandomInitialHeroesUseCase.kt
+++ domain/src/main/kotlin/com/anenha/superhero/domain/usecase/GetRandomInitialHeroesUseCase.kt
@@ -3,6 +3,7 @@ package com.anenha.superhero.domain.usecase
 import com.anenha.superhero.domain.model.SuperHero
 import com.anenha.superhero.domain.repository.SuperHeroRepository
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 
 class GetRandomInitialHeroesUseCase(private val repository: SuperHeroRepository) {
@@ -16,7 +17,7 @@ class GetRandomInitialHeroesUseCase(private val repository: SuperHeroRepository)
             async { repository.getHero(id.toString()) }
         }
 
-        val results = deferreds.map { it.await() }
+        val results = deferreds.awaitAll()
         val heroes = results.mapNotNull { it.getOrNull() }
 
         if (heroes.isNotEmpty()) {
